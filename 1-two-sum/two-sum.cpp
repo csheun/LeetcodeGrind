01215bf91bfff3dc5a720011e939f24b19762ba9
ciaoosuuu <89019459+ciaoosuuu@users.noreply.github.com>
2023-12-13 20:40:55 +0800

    Time: 8 ms (78.59%) | Memory: 11.3 MB (25.74%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        vector<int> workingVector = nums;
-        sort(workingVector.begin(), workingVector.end());
-        int left_ptr = 0;
-        int right_ptr = workingVector.size() - 1;
-        while (left_ptr < right_ptr) {
-            int sum = workingVector[left_ptr] + workingVector[right_ptr];
-            if (sum == target) {
-                break;
-            } else if (sum < target) {
-                left_ptr += 1;
+        std::unordered_map<int, int> myMap;
+        for (size_t i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (myMap.find(complement) != myMap.end()) {
+                return {myMap[complement], static_cast<int>(i)};
             } else {
-                right_ptr -= 1;
+                myMap[nums[i]] = static_cast<int>(i);
             }
         }
-        auto left = std::find(nums.begin(), nums.end(), workingVector[left_ptr]);
-        int index = std::distance(nums.begin(), left);
-        result.push_back(index);
-        auto right = std::find(nums.begin(), nums.end(), workingVector[right_ptr]);
-        if (workingVector[left_ptr] == workingVector[right_ptr]) {
-            right = std::find(left + 1, nums.end(), workingVector[right_ptr]);
-        }
-        index = std::distance(nums.begin(), right);
-        result.push_back(index);
-        return result;
+        return {-1, -1};
     }
-    
 };
