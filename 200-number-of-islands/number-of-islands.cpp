f6a42824f62a38d507048e4b633f407d60a7241e
Chen Shun <89019459+ciaoosuuu@users.noreply.github.com>
2024-09-28 21:56:19 +0800

    Time: 80 ms (9.12%) | Memory: 23.2 MB (27.96%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int count = 0;
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (visited[i][j] || grid[i][j] == '0') 
+                    continue;
+                count += 1;
+                bfs(grid, visited, i, j);
+            }
+        }
+        return count;
+    }
+    void bfs(vector<vector<char>>& grid , vector<vector<bool>>& visited, int i, int j) {
+        // start at i, j
+        // search up down left right
+        int n_row = grid.size();
+        int n_col = grid[0].size();
+        queue<pair<int, int>> q;
+        q.push({i,j});
+        while (!q.empty()) {
+            auto target = q.front();
+            int row = target.first;
+            int col = target.second;
+            cout << "row: " << row << " col: " << col << endl;
+            if (row - 1 >= 0) {
+                if (!visited[row-1][col] && grid[row - 1][col] == '1') {
+                    visited[row - 1][col] = true;
+                    q.push({row - 1, col});
+                }
+            }
+            if (row + 1 < n_row) {
+                if (!visited[row + 1][col] && grid[row + 1][col] == '1') {
+                    visited[row + 1][col] = true;
+                    q.push({row + 1, col});
+                }
+            }
+            if (col - 1 >= 0) {
+                if (!visited[row][col - 1] && grid[row][col - 1] == '1') {
+                    visited[row][col - 1] = true;
+                    q.push({row, col - 1});
+                }
+            }
+            if (col + 1 < n_col) {
+                if (!visited[row][col + 1] && grid[row][col + 1] == '1') {
+                    visited[row][col + 1] = true;
+                    q.push({row, col + 1});
+                }
+            }
+            q.pop();
+        }
+    }
+};
