c708585f8b8d875a626a1c05623344e4e27a2ff8
Chen Shun <89019459+ciaoosuuu@users.noreply.github.com>
2024-10-06 00:41:45 +0800

    Time: 82 ms (64.42%) | Memory: 64.4 MB (74.88%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        unordered_map<int, int>remainder_count_map;
-        int mid{0};
-        if (k % 2 == 0) {
-            mid = k / 2;
-        }
-        for (int num : arr) {
-            int remainder = num % k;
-            remainder_count_map[remainder] += 1;
-        }
-        auto og_map = remainder_count_map;
-        for (auto& x : og_map) {
-            int key = x.first;
-            if (remainder_count_map[key] == -1) {
-                continue;
-            }
-            if (key == 0) {
-                if (x.second % 2 != 0) {
-                    return false;
-                }
-            } else if (key == mid || key == -mid) {
-                if ((remainder_count_map[mid] + remainder_count_map[-mid]) % 2 != 0) {
-                    return false;
-                }
-                remainder_count_map[mid] = -1;
-                remainder_count_map[-mid] = -1;
+        vector<int> rem(k, 0);
+        int count = 0;
+        
+        for(int& i : arr){
+            int r = ((i % k) + k) % k;
+            if (rem[r] > 0) {
+                count++;
+                rem[r]--;
             } else {
-                int x_1 = abs(key);
-                int x_2 = k - x_1;
-                if (remainder_count_map[x_1] + remainder_count_map[-x_2] != remainder_count_map[-x_1] + remainder_count_map[x_2]) {
-                    return false;
-                }
-                remainder_count_map[x_1] = -1;
-                remainder_count_map[x_2] = -1;
-                remainder_count_map[-x_1] = -1;
-                remainder_count_map[-x_2] = -1;
+                rem[(k - r) % k]++;
             }
         }
-        return true;
+        return count == (arr.size() / 2);
     }
 };
