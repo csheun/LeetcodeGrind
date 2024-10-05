26b205619bb535499c3800ff8ab6a76ff9cb22a9
Chen Shun <89019459+ciaoosuuu@users.noreply.github.com>
2024-10-06 00:29:47 +0800

    Time: 263 ms (5.03%) | Memory: 89 MB (5.18%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int, int>remainder_count_map;
+        int mid{0};
+        if (k % 2 == 0) {
+            mid = k / 2;
+        }
+        for (int num : arr) {
+            int remainder = num % k;
+            remainder_count_map[remainder] += 1;
+        }
+        auto og_map = remainder_count_map;
+        for (auto& x : og_map) {
+            cout << "key: " << x.first << " val: " << x.second << '\n';
+            int key = x.first;
+            if (remainder_count_map[key] == -1) {
+                continue;
+            }
+            if (key == 0) {
+                if (x.second % 2 != 0) {
+                    return false;
+                }
+            } else if (key == mid || key == -mid) {
+                if ((remainder_count_map[mid] + remainder_count_map[-mid]) % 2 != 0) {
+                    return false;
+                }
+                remainder_count_map[mid] = -1;
+                remainder_count_map[-mid] = -1;
+            } else {
+                int x_1 = abs(key);
+                int x_2 = k - x_1;
+                if (remainder_count_map[x_1] + remainder_count_map[-x_2] != remainder_count_map[-x_1] + remainder_count_map[x_2]) {
+                    return false;
+                }
+                remainder_count_map[x_1] = -1;
+                remainder_count_map[x_2] = -1;
+                remainder_count_map[-x_1] = -1;
+                remainder_count_map[-x_2] = -1;
+            }
+        }
+        return true;
+    }
+};
