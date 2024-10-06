4251dbc84bd89dadc0c7d7802840ab4be787090b
Chen Shun <89019459+ciaoosuuu@users.noreply.github.com>
2024-10-06 23:21:25 +0800

    Time: 3 ms (45.16%) | Memory: 8.3 MB (76.50%) - LeetSync

---
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        if (sentence1.length() == sentence2.length()) {
+            return sentence1 == sentence2;
+        }
+        if (sentence1.length () > sentence2.length()) {
+            string temp = sentence2;
+            sentence2 = sentence1;
+            sentence1 = temp;
+        }
+        vector<string> sen1_arr = tokenise(sentence1);
+        vector<string> sen2_arr = tokenise(sentence2);
+
+        if (sen1_arr[0] == sen2_arr[0]) {
+            int sen1_idx = 0;
+            for (const string& str : sen2_arr) {
+                if (str == sen1_arr[sen1_idx]) {
+                    sen1_idx++;
+                    if (sen1_idx == sen1_arr.size()) {
+                        return true;
+                    }
+                } else {
+                    break;
+                }
+            }
+            cout << "reached here" << endl;
+            // cannot find cons
+            cout << "sen1_idx: " << sen1_idx << endl; 
+            int j = sen1_arr.size() - 1;
+            for (int i = sen2_arr.size() - 1; i >= 0; i--) {
+                cout << "sen2: " << sen2_arr[i] << '\n';
+                cout << "sen1: " << sen1_arr[j] << '\n';
+                cout << "j: " << j << '\n';
+                if (sen2_arr[i] == sen1_arr[j]) {
+                    if (j == sen1_idx) {
+                        return true;
+                    } 
+                    j--;
+                } else {
+                    return false;
+                }
+            }
+            return false;
+
+        } else {
+            int sen1_idx = sen1_arr.size() - 1;
+            for (int i = sen2_arr.size() - 1; i >= 0; i--) {
+                cout << "sen2_arr: " << sen2_arr[i] << '\n';
+                cout << "sen1_arr: " << sen1_arr[sen1_idx] << '\n';
+                if (sen2_arr[i] == sen1_arr[sen1_idx]) {
+                    if (sen1_idx == 0) {
+                        return true;
+                    }
+                    sen1_idx--;
+                } else {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+    vector<string> tokenise(string sentence) {
+        vector<string> res;
+        string str;
+        for (const char& c : sentence) {
+            if (c == ' ') {
+                res.push_back(str);
+                str = "";
+            } else {
+                str += c;
+            }
+        } 
+        res.push_back(str);
+        return res;
+    }
+};
