4d7f8e24640f6c1e8c30cc66b3b1bae81c344229
Chen Shun <89019459+ciaoosuuu@users.noreply.github.com>
2025-01-14 18:03:57 +0800

    Time: 6 ms (31.97%) | Memory: 9.9 MB (28.54%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,22 +1,19 @@
+#include <unordered_map>
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) {
-            return false;
+        if (s.length() != t.length()) return false;
+        unordered_map<char, int> map;
+        for (int i = 0; i < s.length(); i++) {
+            map[s[i]] += 1;
         }
-        std::unordered_map<char, int> mp;
-        for (char c : s) {
-            if (mp.find(c) != mp.end()) {
-                mp[c] += 1;
-            } else {
-                mp[c] = 1;
-            }
-        }
-        for (char c: t) {
-            if (mp.find(c) == mp.end() || mp[c] == 0) {
+        for (int i = 0; i < t.length(); i++) {
+            if (map.find(t[i]) == map.end()) {
                 return false;
+            } else {
+                map[t[i]]--;
+                if (map[t[i]] < 0) return false;
             }
-            mp[c] -= 1;
         }
         return true;
     }
