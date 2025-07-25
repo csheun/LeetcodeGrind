6bf8d353b854d78a47a2d6246dd51a1bd05dbd9a
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-07-26 00:41:26 +0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.76%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
@@ -2,15 +2,12 @@ class Solution:
     def maxSum(self, nums: List[int]) -> int:
         nums = set(nums)
         res = 0
-        least_nvg = float(-inf)
-        count = 0
+        pos_flag = False
         for num in nums:
             if num > 0:
                 res += num
-                count += 1
-            else:
-                least_nvg = max(least_nvg, num)
-        if count == 0:
-            return least_nvg
+                pos_flag = True
+        if not pos_flag:
+            return max(nums)
         return res
         
