7fc214624bb0f3eb8f4c4370f5ec2dd1a2f8dbc1
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-06 19:41:36 +0800

    Time: 11 ms (49.76%) | Memory: 19.6 MB (20.68%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -2,10 +2,7 @@ from collections import Counter
 class Solution:
     def frequencySort(self, s: str) -> str:
         freq = dict(Counter(s))
-        print(freq)
         freq = dict(sorted(freq.items(), key=lambda x: x[1], reverse=True))
-        print(freq)
-        # freq.sort(reverse=true)
 
         res_lst = []
         for key, value in freq.items():
