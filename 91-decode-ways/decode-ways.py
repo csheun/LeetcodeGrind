f10faaa4aa8cd6ae27f578765284753122b155d6
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-14 17:53:34 +0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (52.41%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,44 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        '''
+        1) recursion - look at the first digit if can then check the
+        remaining string + look at the first 2 digit (TIME LIMIT EXCEEDED)
+        '''
+        # if not s:
+        #     return 1
+        # if (int(s[0]) == 0):
+        #     return 0
+        # if (len(s) == 1):
+        #     return 1
+        # if (int(s[0:2]) <= 26):
+        #     return self.numDecodings(s[1:]) + self.numDecodings(s[2:])
+        # else:
+        #     return self.numDecodings(s[1:])
+        '''
+        2) Dp (break into subproblem)
+
+        dp[i] += dp[i - 1] if s[i] != 0
+        dp[i] += dp[i - 1] if s[i-1:i+1] >= 10 <=26
+        '''
+        # dp tracks the ways to decode message[:idx]
+        # ie dp[2] = ways to decode messages of length 2 -> msg[0:2]
+        # ie dp[n] = ways to msgs of length n thefore len(dp) = n + 1
+
+        if int(s[0]) == 0:
+            return 0
+        
+        dp = [0] * (len(s) + 1)
+
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, len(s) + 1):
+            digit_1 = int(s[i - 1])
+            digit_2 = int(s[i-2:i])
+
+            if (digit_1 != 0):
+                dp[i] += dp[i - 1]
+            if (digit_2 >= 10 and digit_2 <= 26):
+                dp[i] += dp[i - 2]
+            
+        return dp[len(s)]
+        
