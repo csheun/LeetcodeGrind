1462fc35ff8b7c93ae8d28c0ae9644fa225e7640
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-14 16:23:07 +0800

    Time: 1506 ms (32.38%) | Memory: 25.5 MB (12.05%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -11,7 +11,6 @@ class Solution:
         dp = [[False] * len(s) for _ in range(len(s))]
 
         for i in range(len(s)):
-            dp[i][i] = True
             for j in range(i + 1):
                 if (s[i] == s[j] and (i-j <= 2 or dp[j+1][i-1])):
                     dp[j][i] = True
