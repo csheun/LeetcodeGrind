7022fb947efd92bf0503c9cbcdbdf927aae8085b
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-14 19:58:37 +0800

    Time: 1764 ms (46.61%) | Memory: 18.3 MB (19.65%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -9,12 +9,10 @@ class Solution:
 
         # will minimum be 1 as it can start its own subsequence
         dp = [1] * len(nums)
-        res = 1
         for i in range(len(nums)):
             for j in range(i):
                 if (nums[j] < nums[i]):
                     dp[i] = max(dp[i], dp[j] + 1)
-                    res = max(res, dp[i])
         
-        return res
+        return max(dp)
         
