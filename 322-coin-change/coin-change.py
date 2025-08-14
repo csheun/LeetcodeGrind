63ec9b9a3cb30b27b9fd4ca27f290d50d7806678
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-14 18:31:10 +0800

    Time: 784 ms (46.67%) | Memory: 18.3 MB (43.44%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -16,22 +16,37 @@ class Solution:
         #         # else: -ve res -> dont do anything
         # return -1 
                 
-        # bfs with memoization
+        # # bfs with memoization (works)
+        # if amount == 0:
+        #     return 0
+        # visited_amt = set()
+        # visited_amt.add(amount)
+        # queue = deque()
+        # queue.append(tuple([amount, 0]))
+        # while queue:
+        #     amt, cnt = queue.popleft()
+        #     for coin in coins:
+        #         diff = amt - coin
+        #         if diff > 0 and diff not in visited_amt:
+        #             visited_amt.add(diff)
+        #             queue.append(tuple([diff, cnt + 1]))
+        #         elif diff == 0:
+        #             return cnt + 1
+        #         # else: -ve res -> dont do anything
+        # return -1 
+
+        '''
+        dp solution:
+        dp[i] = minimum coin change to range amount i
+        '''
+
         if amount == 0:
             return 0
-        visited_amt = set()
-        visited_amt.add(amount)
-        queue = deque()
-        queue.append(tuple([amount, 0]))
-        while queue:
-            amt, cnt = queue.popleft()
+        dp = [math.inf] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
             for coin in coins:
-                diff = amt - coin
-                if diff > 0 and diff not in visited_amt:
-                    visited_amt.add(diff)
-                    queue.append(tuple([diff, cnt + 1]))
-                elif diff == 0:
-                    return cnt + 1
-                # else: -ve res -> dont do anything
-        return -1 
-        
+                if (i - coin >= 0):
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        return dp[amount] if dp[amount] != math.inf else -1
