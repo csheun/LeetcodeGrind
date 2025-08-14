da3dc63381c46bd2ad9f8f4297973efde23a92a4
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-14 20:10:01 +0800

    Time: 3 ms (95.73%) | Memory: 18.1 MB (38.90%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,19 +1,19 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        '''
-        dp[i] = length of longest subsequence end with nums[i]
-        need to check all indexes infront:
-        if nums[x] < nums[i] then dp[i] = dp[x] + 1,
-        take the maximum possible value
-        ''' 
+        # '''
+        # dp[i] = length of longest subsequence end with nums[i]
+        # need to check all indexes infront:
+        # if nums[x] < nums[i] then dp[i] = dp[x] + 1,
+        # take the maximum possible value
+        # ''' 
 
-        # will minimum be 1 as it can start its own subsequence
-        dp = [1] * len(nums)
-        for i in range(len(nums)):
-            for j in range(i):
-                if (nums[j] < nums[i]):
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp)
+        # # will minimum be 1 as it can start its own subsequence
+        # dp = [1] * len(nums)
+        # for i in range(len(nums)):
+        #     for j in range(i):
+        #         if (nums[j] < nums[i]):
+        #             dp[i] = max(dp[i], dp[j] + 1)
+        # return max(dp)
 
         '''
         Binary search (recommended way)
