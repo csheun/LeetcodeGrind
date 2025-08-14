c940c54cc29d52e9a3ce978b41dae82a3a0510d1
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-15 03:25:16 +0800

    Time: 51 ms (40.15%) | Memory: 19.9 MB (56.70%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,17 +6,29 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        hash_table = {}
-        while head:
-            if head.val not in hash_table:
-                hash_table[head.val] = []
-                hash_table[head.val].append(head)
-            else:
-                # loop through arr to see if same
-                for node in hash_table[head.val]:
-                    if head is node:
-                        return True
-                hash_table[head.val].append(head)
-            head = head.next
+        # # hash_table method (works)
+        # hash_table = {}
+        # while head:
+        #     if head.val not in hash_table:
+        #         hash_table[head.val] = []
+        #         hash_table[head.val].append(head)
+        #     else:
+        #         # loop through arr to see if same
+        #         for node in hash_table[head.val]:
+        #             if head is node:
+        #                 return True
+        #         hash_table[head.val].append(head)
+        #     head = head.next
+        # return False
+
+        # optimal solution
+        # use slow and fast pointers: slow will move 1 step fast will move 2 steps, fast will eventually   catch up to slow
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if (fast is slow):
+                return True
         return False
             
