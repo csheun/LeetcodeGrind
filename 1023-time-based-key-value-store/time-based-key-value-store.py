bb7171240b1ef4b597555f57bbde69ed6c4d0215
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-17 20:41:23 +0800

    Time: 136 ms (40.06%) | Memory: 73.8 MB (86.91%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -11,15 +11,33 @@ class TimeMap:
 
 
     def get(self, key: str, timestamp: int) -> str:
+        print(f"searching for key: {key} timestamp: {timestamp}")
         arr = self.key_val_map.get(key)
         if not arr:
+            print("Array not found")
             return ""
         # search through arr for the best timestamp
-        # brute force
-        for i in range(len(arr) - 1, -1, -1):
-            if (arr[i][0] <= timestamp):
-                return arr[i][1]
-        return ""
+        # # brute force (works but nvr use binary search)
+        # for i in range(len(arr) - 1, -1, -1):
+        #     if (arr[i][0] <= timestamp):
+        #         return arr[i][1]
+        # return ""
+
+        # binary search
+        lo = 0
+        hi = len(arr) -1
+        ans = ""
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if (arr[mid][0] == timestamp):
+                return arr[mid][1]
+            elif (arr[mid][0] > timestamp):
+                hi = mid - 1
+            else:
+                ans = arr[mid][1]
+                lo = mid + 1
+        
+        return ans
 
         
 
