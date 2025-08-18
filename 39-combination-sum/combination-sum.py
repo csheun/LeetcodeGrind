3c5edcf3f67fe4d748e68e10d106201ec1cdf1ed
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-18 20:06:49 +0800

    Time: 10 ms (61.36%) | Memory: 17.7 MB (87.79%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def helper(idx: int, total_sum: int, arr: List[int]):
+            if (total_sum == target):
+                res.append(arr)
+            elif (total_sum < target):
+                for i in range(idx, len(candidates)):
+                    new_sum = total_sum + candidates[i]
+                    helper(i, new_sum, arr + [candidates[i]])
+        res = []
+        helper(0, 0, [])
+        return res
+        
