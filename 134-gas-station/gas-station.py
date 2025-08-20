04f5a89c24757afcd0e470341d6719bf7d5bdb63
Chen Shun <89019459+csheun@users.noreply.github.com>
2025-08-20 17:19:23 +0800

    Time: 37 ms (23.27%) | Memory: 23.2 MB (83.52%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -8,19 +8,35 @@ class Solution:
         # B + C will always be < A + B + C as A is +ve (if not the condition will fail early) so if A + B + C + D is -ve then B + C + D is -ve so 
         # point A, B, C, D all cannot be starting point
 
-        diff = [gas[i] - cost[i] for i in range(len(gas))]
-        if sum(diff) < 0:
-            return -1
+        # diff = [gas[i] - cost[i] for i in range(len(gas))]
+        # if sum(diff) < 0:
+        #     return -1
+        # curr_gas = 0
+        # i = 0
+        # n = len(diff)
+        # while i < n:
+        #     curr_gas = 0
+        #     for j in range(n):
+        #         curr_gas += diff[(i + j) % n]
+        #         if curr_gas < 0:
+        #             i = (i + j) % n + 1
+        #             break
+        #     if curr_gas >= 0:
+        #         return i 
+        # return -1
+
+
+        # cleaner way
+        n = len(gas)
+        total_gas = total_cost = 0
         curr_gas = 0
-        i = 0
-        n = len(diff)
-        while i < n:
-            curr_gas = 0
-            for j in range(n):
-                curr_gas += diff[(i + j) % n]
-                if curr_gas < 0:
-                    i = (i + j) % n + 1
-                    break
-            if curr_gas >= 0:
-                return i 
-        return -1
+        start_idx = 0
+        for i in range(n):
+            total_gas += gas[i]
+            total_cost += cost[i]
+            curr_gas += gas[i] - cost[i]
+            if curr_gas < 0:
+                start_idx = i + 1
+                curr_gas = 0
+        return -1 if total_gas < total_cost else start_idx
+
